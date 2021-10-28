04845670d953780e3cc46dcbc160345e13831c8d
Todd Volkert <tvolkert@google.com>
2021-10-28 11:46:34 -0700

    Simplified test case

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 
 void main() async {
-  runApp(Image.asset('assets/image.jpg'));
-  await Future.delayed(const Duration(seconds: 1));
-  runApp(Container());
+  runApp(const ColoredBox(color: Color(0xffff0000)));
+  runApp(const LimitedBox());
 }
